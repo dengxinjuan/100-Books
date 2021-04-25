c305c2e455da941e55291113ede89b191a13b460
xinjuan deng <dengxinj@gmail.com>
2021-04-24 22:21:56 -0400

    add a footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+import Button from "./Button";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <h2>Created by Xinjuan Deng</h2>
+      <small class="website-rights">100Books © 2021</small>
+    </div>
+  );
+}
+
+export default Footer;
