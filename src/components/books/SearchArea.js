2472b0760db96611543e23ce8463d4c9d2ee1903
xinjuan deng <dengxinj@gmail.com>
2021-05-11 15:53:23 -0400

    add search area

--- src/components/books/SearchArea.js
+++ src/components/books/SearchArea.js
@@ -2,10 +2,7 @@ import { useState } from "react";
 
 /*style*/
 import { makeStyles } from "@material-ui/core/styles";
-
-import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Input from "@material-ui/core/Input";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 
