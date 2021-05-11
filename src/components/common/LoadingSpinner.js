380caf7571c0bb0395f219f9d03d34bcf447216b
xinjuan deng <dengxinj@gmail.com>
2021-05-11 14:05:59 -0400

    add loading image

--- src/components/common/LoadingSpinner.js
+++ src/components/common/LoadingSpinner.js
@@ -4,7 +4,15 @@ import "./LoadingSpinner.css";
 /** Loading message used by components that fetch API data. */
 
 function LoadingSpinner() {
-  return <div className="LoadingSpinner">Loading ...</div>;
+  return (
+    <div className="LoadingSpinner">
+      Loading ...
+      <img
+        src="https://miro.medium.com/max/2400/1*kW-mFsR5zjhrpmfmLRdSrQ.png"
+        alt="loading"
+      />
+    </div>
+  );
 }
 
 export default LoadingSpinner;
